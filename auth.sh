908fcfb6606dca4ad90739e971d839b3649b35b5
Ricardo Baumann <ricardo.luis.baumann@gmail.com>
2018-08-20 11:21:34 +0200

    wip

---
+++ auth.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+curl -X POST -vu clientapp:123456 http://localhost:9000/oauth/token -H "Accept: application/json" -d "password=admin&username=admin&grant_type=password&scope=read%20write&client_secret=123456&client_id=clientapp"
